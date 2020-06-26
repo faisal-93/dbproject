4d4f570e949627eeed86af0357f0d5caffb31622
Farruck Ahamed Tusar <frk.tusar@gmail.com>
2020-06-26 03:18:44 +0200

    Add datatables lib to frontend

--- index.php
+++ index.php
@@ -1,43 +1,53 @@
-<?php 
-
-?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Datenbanken Project</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<title>Datenbanken Project</title>	
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
+		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		
+		<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+		<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+		
 	</head>
 	<body>
 		<div class="container">
+			<br />
 			<h3 align="center">Datenbanken Project</h3>
 			<br />
-
-			
-			
-			<div align="right" style="margin-bottom:5px;">
-				<button type="button" name="add_button" id="add_button" class="btn btn-success btn-xs">Add</button>
-			</div>
-
 			<div class="table-responsive">
-				<table class="table table-bordered table-striped">
+				<table id="example" class="table table-striped table-bordered" style="width:100%">
 					<thead>
 						<tr>
 							<th>IP</th>
 							<th>Port</th>
+							<th>Country Code</th>
+							<th>Country</th>
+							<th>Protocol</th>
+							<th>Last Update Time</th>
 						</tr>
 					</thead>
 					<tbody></tbody>
 				</table>
 			</div>
-			
 		</div>
 	</body>
 </html>
 
 <script type="text/javascript">
+
 $(document).ready(function(){
+
+	$('#example').DataTable({
+        paging: true,
+        scrollX: true,
+        lengthChange : true,
+        searching: true,
+        ordering: true
+      });
 
 	fetch_data();
 
--- proxy/fetch.php
+++ proxy/fetch.php
@@ -21,6 +21,10 @@ if(count($result) > 0)
 		<tr>
 			<td>'.$row->ip.'</td>
 			<td>'.$row->port.'</td>
+			<td>'.$row->country_code.'</td>
+			<td>'.$row->country.'</td>
+			<td>'.($row->type == '0'? "HTTP" : "HTTPS").'</td>
+			<td>'.$row->updated_at.'</td>
 		</tr>
 		';
 	}
@@ -29,7 +33,7 @@ else
 {
 	$output .= '
 	<tr>
-		<td colspan="4" align="center">No Data Found</td>
+		<td colspan="6" align="center">No Data Found</td>
 	</tr>
 	';
 }
